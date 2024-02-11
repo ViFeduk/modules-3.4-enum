02af5ddd1b7d30ca842194a2cf0d9c5b0dfabc6d
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-02-11 18:49:13 +0300

    enum Semaphore addd

--- modules 3.4 enum/Program.cs
+++ modules 3.4 enum/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
+            int semaphoreNumber = (int)Semaphore.yellow;
+            Console.WriteLine( semaphoreNumber);
             
         }
+    }
+
+    enum Semaphore
+    {
+        red= 100, yellow= 100, green=300
     }
 }
